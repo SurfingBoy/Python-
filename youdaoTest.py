7dc40b08e7dc2d54547c1ae1241aff261a48efb3
SurfingBoy <a1520221575@163.com>
2018-01-01 03:20:48 +0800

    有道翻译

--- youdaoTest.py
+++ youdaoTest.py
@@ -4,6 +4,9 @@ import json
 url='http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule' \
     '&smartresult=ugc&sessionFrom=http://www.youdao.com/'
 content=input('输入要翻译的内容：')
+head={'Referer':'fanyi.youdao.com',
+      'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+      'Chrome/63.0.3239.108 Safari/537.36X-Requested-With:XMLHttpRequest'}
 data={}
 data['i']=content
 data['type']='AUTO'
@@ -15,7 +18,7 @@ data['version']='2.1'
 data['action']='FY_BY_CLICKBUTTON'
 data['typoResult']='true'
 data=urllib.parse.urlencode(data).encode('utf-8')
-response=urllib.request.urlopen(url,data)
+response=urllib.request.urlopen(url,data,head)
 html=response.read().decode('utf-8')
 target=json.loads(html)
 print('翻译结果：%s'%(target['translateResult'][0][0]['tgt']))

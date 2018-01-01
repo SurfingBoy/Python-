8a65e58edb121595237a848bad5bdc8e0d2df7ed
SurfingBoy <a1520221575@163.com>
2018-01-01 13:00:12 +0800

    获取猫的图片

--- WebScrapTest1.py
+++ WebScrapTest1.py
@@ -1,5 +1,5 @@
 import urllib.request
-response=urllib.request.urlopen('http://placekitten.com/g/400/400')  #获取猫的图片
-cat_img=response.read()
-with open('400.jpg','wb') as f:
+response = urllib.request.urlopen('http://placekitten.com/g/400/400')  #获取猫的图片
+cat_img = response.read()
+with open('400.jpg', 'wb') as f:
     f.write(cat_img)

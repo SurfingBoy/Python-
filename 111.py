33b762f9607dc1efb38df5cbb96f92eabe146fe1
SurfingBoy <a1520221575@163.com>
2018-01-26 13:18:58 +0800

    python基础

--- 111.py
+++ 111.py
@@ -414,29 +414,29 @@
 #--------------Pool---------------
 
 #---------------进程间通信----------------
-from multiprocessing import Process,Queue
-import os,time,random
-
-def write(q):
-    print("process to write: %s"%os.getpid())
-    for value in ['A','B','C']:
-        print('Put %s to queue..'%value)
-        q.put(value)
-        time.sleep(random.random())
-def read(q):
-    print('Process to read:%s'%os.getpid())
-    while True:
-        value=q.get(True)
-        print('Get %s from queue'%value)
-if __name__ == '__main__':
-    q=Queue()
-    pw=Process(target=write,args=(q,))
-    pr=Process(target=read,args=(q,))
-    pw.start()
-    pr.start()
-    #等待pw进程结束
-    pw.join()
-    #pr进程里是死循环，无法等待其结束，只能强行终止
-    pr.terminate()
+# from multiprocessing import Process,Queue
+# import os,time,random
+#
+# def write(q):
+#     print("process to write: %s"%os.getpid())
+#     for value in ['A','B','C']:
+#         print('Put %s to queue..'%value)
+#         q.put(value)
+#         time.sleep(random.random())
+# def read(q):
+#     print('Process to read:%s'%os.getpid())
+#     while True:
+#         value=q.get(True)
+#         print('Get %s from queue'%value)
+# if __name__ == '__main__':
+#     q=Queue()
+#     pw=Process(target=write,args=(q,))
+#     pr=Process(target=read,args=(q,))
+#     pw.start()
+#     pr.start()
+#     #等待pw进程结束
+#     pw.join()
+#     #pr进程里是死循环，无法等待其结束，只能强行终止
+#     pr.terminate()
 #---------------进程间通信----------------
 #----------------------进程----------------------

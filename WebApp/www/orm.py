8c200ed1ffb4f8d4bc846bba4f93df2575b6e6fe
SurfingBoy <a1520221575@163.com>
2018-02-10 16:28:53 +0800

    第二天

--- WebApp/www/orm.py
+++ WebApp/www/orm.py
@@ -69,8 +69,8 @@ class Field(object):
     def __str__(self):
         return '<%s,%s:%s>'%(self.__class__.__name__,self.column_type,self.name)
 class StringField(Field):
-    def __init__(self,name=None,primary_key=False,default=None,dd1='varchar(100)'):
-        super().__init__(name,dd1,primary_key,default)
+    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
+        super().__init__(name,ddl,primary_key,default)
 class BooleanField(Field):
     def __init__(self,name=None,default=False):
         super().__init__(name,'boolean',False,default)
